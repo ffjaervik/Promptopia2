1ff5dca04d10635d35c3254130a82ed0503a80e0
ffjaervik <fabianfjaervik@gmail.com>
2023-06-13 14:24:08 +0100

    desktop navigation

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
-
-
 export default function Home() {
   return (
-    <section>
-      <h1>
-            Discover and Share
-            <br />
-            <span>AI-Powered Prompts</span>
+    <section className="w-full flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className=" max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts.
+      </p>
     </section>
-  )
+  );
 }
---
+++ components/Nav.jsx
@@ -0,0 +1,65 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+export default function Nav() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  return (
+    <nav className="w-full pt-3 mb-16 flex-between">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src=" /assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia 2</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className="hidden sm:flex">
+        {isSignedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button
+              type="button"
+              onClick={() => {
+                setIsSignedIn((current) => !current);
+              }}
+              className="outline_btn"
+            >
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src=" /assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rouded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            <button
+              type="button"
+              onClick={() => {
+                setIsSignedIn((current) => !current);
+              }}
+            >
+              Sign In
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
