df7836e32950bcc4568f004ce9899a43ba2e4cc4
ffjaervik <fabianfjaervik@gmail.com>
2023-06-13 12:38:41 +0100

    layout, css, tailwind, folder structure

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <section>
+      <h1>
+            Discover and Share
+            <br />
+            <span>AI-Powered Prompts</span>
+      </h1>
+    </section>
+  )
+}
